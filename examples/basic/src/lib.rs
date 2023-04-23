922ae26d7175a36813b8817301481ab1a2967668
Eugene Chou <euchou@ucsc.edu>
2023-04-23 03:12:00 +0000

    fix segfault on linux with custom print macros

--- examples/basic/src/lib.rs
+++ examples/basic/src/lib.rs
@@ -1,17 +1,10 @@
-use shimmer::{shimmer, shimmer_hook};
+use shimmer::prelude::*;
 
 #[shimmer]
 #[derive(Default)]
 struct State {}
 
 trait BasicIO {
-    unsafe fn read(
-        &mut self,
-        fd: libc::c_int,
-        buf: *mut libc::c_void,
-        nbytes: libc::size_t,
-    ) -> libc::c_int;
-
     unsafe fn write(
         &mut self,
         fd: libc::c_int,
@@ -22,21 +15,12 @@ trait BasicIO {
 
 #[shimmer_hook]
 impl BasicIO for State {
-    unsafe fn read(
-        &mut self,
-        fd: libc::c_int,
-        buf: *mut libc::c_void,
-        nbytes: libc::size_t,
-    ) -> libc::c_int {
-        println!("[read] fd={fd}, size={nbytes}");
-    }
-
     unsafe fn write(
         &mut self,
         fd: libc::c_int,
         buf: *mut libc::c_void,
         nbytes: libc::size_t,
     ) -> libc::c_int {
-        println!("[write] fd={fd}, size={nbytes}");
+        shimmer_println!("[write] fd={fd}, size={nbytes}");
     }
 }
