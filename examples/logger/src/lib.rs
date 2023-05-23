00581ed887268e09e6aee2a3752abb940023c0d2
Dev <dpuranda@ucsc.edu>
2023-05-23 23:13:11 +0000

    Logger with paths and pread/pwrite

--- examples/logger/src/lib.rs
+++ examples/logger/src/lib.rs
@@ -1,6 +1,8 @@
 use log::*;
 use shimmer::prelude::*;
 use simple_logger::SimpleLogger;
+use std::fs::read_link;
+use std::path::PathBuf;
 
 #[shimmer]
 struct State {}
@@ -21,6 +23,56 @@ impl State {
         buf: *mut libc::c_void,
         nbytes: libc::size_t,
     ) -> libc::c_int {
-        info!("[write] fd={fd}, size={nbytes}");
+        info!("[write] path={} size={nbytes}", fd_to_path(fd));
     }
+    unsafe fn read(
+        &mut self,
+        fd: libc::c_int,
+        buf: *mut libc::c_void,
+        nbytes: libc::size_t,
+    ) -> libc::c_int {
+        info!("[read] path={} size={nbytes}", fd_to_path(fd));
+    }
+    unsafe fn pread(
+        &mut self,
+        fd: libc::c_int,
+        buf: *mut libc::c_void,
+        nbytes: libc::size_t,
+        offset: libc::off_t,
+    ) -> libc::c_int {
+        info!("[pread] path={} size={nbytes}", fd_to_path(fd));
+    }
+    unsafe fn pwrite(
+        &mut self,
+        fd: libc::c_int,
+        buf: *mut libc::c_void,
+        nbytes: libc::size_t,
+        offset: libc::off_t,
+    ) -> libc::c_int {
+        info!("[pwrite] path={} size={nbytes}", fd_to_path(fd));
+    }
+    unsafe fn pread64(
+        &mut self,
+        fd: libc::c_int,
+        buf: *mut libc::c_void,
+        nbytes: libc::size_t,
+        offset: libc::off_t,
+    ) -> libc::c_int {
+        info!("[pread64] path={} size={nbytes}", fd_to_path(fd));
+    }
+    unsafe fn pwrite64(
+        &mut self,
+        fd: libc::c_int,
+        buf: *mut libc::c_void,
+        nbytes: libc::size_t,
+        offset: libc::off_t,
+    ) -> libc::c_int {
+        info!("[pwrite64] path={} size={nbytes}", fd_to_path(fd));
+    }
+}
+
+fn fd_to_path(fd: libc::c_int) -> String {
+    let path_fd = PathBuf::from(format!("/proc/self/fd/{}", fd));
+    let file_name = read_link(path_fd).unwrap();
+    return file_name.to_str().unwrap().to_string();
 }
